c21ada715f840a02b2d7a0f3f710b68467479963
German <gtumorin@gmail.com>
2023-07-16 20:22:45 +0200

    form SignIn install react-hook-form for control and validation

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -1,53 +1,71 @@
 import { React, useState } from "react";
 import { ReactComponent as UserIcon } from "../assets/icons/person.svg";
 import { ReactComponent as PasswordIcon } from "../assets/icons/password.svg";
+import { ReactComponent as EmailIcon } from "../assets/icons/email.svg";
 
 const SignInForm = () => {
-  const [user, setUser] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleChange = (e) => {
     const target = e.target;
     const value = target.value;
-    // const name = target.name;
-    setUser(value);
+    const name = target.name;
+    name === "email" ? setEmail(value) : setPassword(value);
   };
 
   // handleSubmit(event) {
   //   alert('Submit');
   //   event.preventDefault();
   // }
-
   return (
     <div className="wrapper">
       <div className="signIn_container">
         <form className="signIn_form">
           <div className="user_info">
-            <label className="user_name">
-              <UserIcon />
-            </label>
-            <input
-              className="input_user"
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Name"
-              value={user}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="password">
-            <label className="label_password">
-              <PasswordIcon />
-            </label>
-            <input
-              className="input_password"
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Password"
-              // onChange={handleChange}
-            />
+            <div className="user_name">
+              <label className="label_name">
+                <UserIcon />
+              </label>
+              <input
+                className="input_name"
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Name"
+                value={name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="user_email">
+              <label className="label_email">
+                <EmailIcon />
+              </label>
+              <input
+                className="input_email"
+                type="text"
+                name="email"
+                id="email"
+                placeholder="Email"
+                value={email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="user_password">
+              <label className="label_password">
+                <PasswordIcon />
+              </label>
+              <input
+                className="input_password"
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                value={password}
+                onChange={handleChange}
+              />
+            </div>
           </div>
           <div>
             <input className="submit_by_name" type="submit" value="Sign in" />
