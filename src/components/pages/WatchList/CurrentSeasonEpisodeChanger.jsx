437d3b8aa7afe46cae3b5c408b4a9f18de8ca5fc
German <gtumorin@gmail.com>
2023-11-16 11:58:24 +0100

    current season change mechanism

--- src/components/pages/WatchList/CurrentSeasonEpisodeChanger.jsx
+++ src/components/pages/WatchList/CurrentSeasonEpisodeChanger.jsx
@@ -1,18 +1,52 @@
-import React from "react";
+import { React, useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 
-const CurrentSeasonEpisodeChanger = () => {
+const CurrentSeasonEpisodeChanger = ({
+  seasons,
+  currentSeason,
+  currentEpisode,
+}) => {
+  const [currSeason, setCurrSeason] = useState(currentSeason);
+  const [currEpisode, setCurrEpisode] = useState(currentEpisode);
+
+  const displayedSeasons = Object.keys(seasons).map(
+    (season) => Number(season) + 1
+  );
+
+  const handleItemsSelected = (eventKey) => {
+    setCurrSeason(eventKey);
+  };
+
   return (
-    <DropdownButton
-      id="dropdown-basic-button"
-      size="sm"
-      title="Dropdown button"
-    >
-      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-    </DropdownButton>
+    <div className="change_currentPosition">
+      <div className="change_currentSeason">
+        <b>Change current season:</b>
+        <DropdownButton
+          id="dropdown-basic-button"
+          size="sm"
+          title={currSeason}
+          onSelect={(eventKey) => handleItemsSelected(eventKey)}
+        >
+          {displayedSeasons.map((season) => (
+            <Dropdown.Item eventKey={season}>{season}</Dropdown.Item>
+          ))}
+        </DropdownButton>
+      </div>
+      <div className="change_currentEpisode">
+        <b>Change current episode:</b>
+        <DropdownButton
+          id="dropdown-basic-button"
+          size="sm"
+          title={currEpisode}
+          onSelect={(eventKey) => handleItemsSelected(eventKey)}
+        >
+          {Object.keys(seasons).map((season) => (
+            <Dropdown.Item eventKey={season}>{season}</Dropdown.Item>
+          ))}
+        </DropdownButton>
+      </div>
+    </div>
   );
 };
 
--- src/components/pages/WatchList/WatchingSeriesItem.jsx
+++ src/components/pages/WatchList/WatchingSeriesItem.jsx
@@ -4,25 +4,30 @@ import Accordion from "react-bootstrap/Accordion";
 import CurrentSeasonEpisodeChanger from "./CurrentSeasonEpisodeChanger";
 
 const WatchingSeriesItem = ({ imdbID }) => {
+  const { currentSeason, currentEpisode, seriesInfo } = imdbID;
+  console.log("seriesInfo", seriesInfo);
+  const { title, seasons } = seriesInfo;
   return (
     <Accordion.Item eventKey={imdbID}>
       <Accordion.Header>
         <div className="watchingSeriesItem_info">
           <div className="watchingSeriesItem_title">
-            <b>{imdbID.seriesInfo.title}</b>
+            <b>{title}</b>
           </div>
           <div className="watchingSeriesItem_currentSeason">
-            Current Season: <b>{imdbID.currentSeason}</b>
+            Current Season: <b>{currentSeason}</b>
           </div>
           <div className="watchingSeriesItem_currentEpisode">
-            Current Episode: <b>{imdbID.currentEpisode}</b>
+            Current Episode: <b>{currentEpisode}</b>
           </div>
         </div>
       </Accordion.Header>
       <Accordion.Body>
-        <div className="change_currentPosition">
-          <CurrentSeasonEpisodeChanger />
-        </div>
+        <CurrentSeasonEpisodeChanger
+          seasons={seasons}
+          currentSeason={currentSeason}
+          currentEpisode={currentEpisode}
+        />
       </Accordion.Body>
     </Accordion.Item>
   );
