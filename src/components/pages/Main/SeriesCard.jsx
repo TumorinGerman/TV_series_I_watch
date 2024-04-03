548a2775c744900998d066c735a8356f5b4ad4bf
German <gtumorin@gmail.com>
2024-04-03 17:48:47 +0200

    Button to add to watch list from rating page

--- src/components/pages/Main/SeriesCard.jsx
+++ src/components/pages/Main/SeriesCard.jsx
@@ -2,13 +2,29 @@ import React from "react";
 import { Card } from "react-bootstrap";
 
 const SeriesCard = (props) => {
+  const addSeriesToWatchListFromRatingPage = () => {
+    console.log("work");
+  };
   return (
     <Card>
       <Card.Img variant="top" src={props.poster} />
       <Card.Header>
-        <div className="imdb_logo">
-          <img src="/img/icons/imdb_small.png" alt="imdb logo"></img>{" "}
-          {props.imdbRating}
+        <div className="card_header">
+          <div className="imdb_info">
+            <div className="imdb_logo">
+              <img src="/img/icons/imdb_small.png" alt="imdb logo"></img>{" "}
+            </div>
+            <div className="imdbRating">{props.imdbRating}</div>
+          </div>
+          <div className="button_addToWL">
+            <button
+              className="button_toWatchList"
+              type="button"
+              onClick={() => addSeriesToWatchListFromRatingPage()}
+            >
+              to WatchList
+            </button>
+          </div>
         </div>
       </Card.Header>
       <Card.Body>
